86fe27ff05cc36794ac5cce55860b917fbdaf4c4
Adam Enmark <aden1800@student.miun.se>
2019-09-06 10:32:48 +0200

    La till 2 stycken views för start meny och själva spelet

--- YatzyGrupp2/View/GameView.xaml.cs
+++ YatzyGrupp2/View/GameView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace YatzyGrupp2
+namespace YatzyGrupp2.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for GameView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class GameView : Window
     {
-        public MainWindow()
+        public GameView()
         {
             InitializeComponent();
         }
