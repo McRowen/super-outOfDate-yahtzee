569aa910773a9abca45267beae2b4d129d4850c4
Petter edlund <petter.edlund@gmail.com>
2019-09-06 12:17:31 +0200

    Petters Test. Provade göra en Listview som har flera collumer så man slipper göra en massa labels

---
+++ YatzyGrupp2/Test/Listview.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace YatzyGrupp2.Test
+{
+    /// <summary>
+    /// Interaction logic for Listview.xaml
+    /// </summary>
+    public partial class Listview : Window
+    {
+        public Listview()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void initListView()
+        {/*
+            // Add columns
+            lvRegAnimals.Columns.Add("Id", -2, HorizontalAlignment.Left);
+            lvRegAnimals.Columns.Add("Name", -2, HorizontalAlignment.Left);
+            lvRegAnimals.Columns.Add("Age", -2, HorizontalAlignment.Left);
+         */
+        }
+
+        private void listView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
