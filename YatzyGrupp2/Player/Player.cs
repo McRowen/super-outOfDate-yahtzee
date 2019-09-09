51b54ff6cdce1e569ec53566054a6011991f1be4
Rasmus Owen <Rasmus.Owen@hotmail.com>
2019-09-09 11:12:05 +0200

    Lagt till props i Player

--- YatzyGrupp2/Player/Player.cs
+++ YatzyGrupp2/Player/Player.cs
@@ -8,5 +8,9 @@ namespace YatzyGrupp2.Player
 {
     class Player
     {
+        public int Player_id { get; private set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Nickname { get; set; }
     }
 }
