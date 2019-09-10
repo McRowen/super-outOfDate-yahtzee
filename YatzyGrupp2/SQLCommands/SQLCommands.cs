91efeafce8d20fd022398dc62a378493be0c937c
Rasmus Owen <Rasmus.Owen@hotmail.com>
2019-09-10 11:00:21 +0200

    Revert "Anslutning mot databas"
    This reverts commit ef1cb4e3330475afb1afef8d4435e07bf1d3f8ab.

--- YatzyGrupp2/SQLCommands/SQLCommands.cs
+++ YatzyGrupp2/SQLCommands/SQLCommands.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Configuration;
-using Npgsql;
 
 
 namespace YatzyGrupp2.SQLCommands
 {
     class SQLCommands
     {
-        NpgsqlConnection conn = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["dbConn"].ConnectionString);
+        Player.Player p = new Player.Player();
     }
 }
