ef1cb4e3330475afb1afef8d4435e07bf1d3f8ab
Rasmus Owen <Rasmus.Owen@hotmail.com>
2019-09-10 10:34:43 +0200

    Anslutning mot databas

--- YatzyGrupp2/SQLCommands/SQLCommands.cs
+++ YatzyGrupp2/SQLCommands/SQLCommands.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using Npgsql;
 
 
 namespace YatzyGrupp2.SQLCommands
 {
     class SQLCommands
     {
-        Player.Player p = new Player.Player();
+        NpgsqlConnection conn = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["dbConn"].ConnectionString);
     }
 }
