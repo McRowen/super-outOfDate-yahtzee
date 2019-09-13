c2a169aebd69c52396e15bd9356e0fcf3a693fe7
k.ebba.lundberg <k.ebba.lundberg@gmail.com>
2019-09-13 11:26:29 +0200

    Revert "Helpview"
    This reverts commit 3b97b9952c95c286ece25b1df826a9fc21b834f7.

--- YatzyGrupp2/View/StartView.xaml.cs
+++ YatzyGrupp2/View/StartView.xaml.cs
@@ -98,11 +98,5 @@ namespace YatzyGrupp2.View
         {
             MessageBox.Show($"Du har nu {click} spelare i ditt spel, då spelar vi.");
         }
-
-        private void Help_Click(object sender, RoutedEventArgs e)
-        {
-            HelpView helpview = new HelpView();
-            helpview.ShowDialog(); 
-        }
     }
 }
