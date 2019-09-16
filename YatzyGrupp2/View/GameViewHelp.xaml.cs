05d596948e1d8da2b3061734287d80d4814f73ec
k.ebba.lundberg <k.ebba.lundberg@gmail.com>
2019-09-16 12:02:33 +0200

    Lägger in regler i GameViewHelp

--- YatzyGrupp2/View/GameViewHelp.xaml.cs
+++ YatzyGrupp2/View/GameViewHelp.xaml.cs
@@ -22,11 +22,24 @@ namespace YatzyGrupp2.View
         public GameViewHelp()
         {
             InitializeComponent();
+            GameRules();
+        
         }
 
         private void BtnOK_Click(object sender, RoutedEventArgs e)
         {
             this.Hide();
+        }
+
+        public string GameRules()
+        {
+            return gameRules.Text = "På skärmen kommer det visas tydligt vems tur det är. " +
+                "Varje gång det blir din tur har du rätt till högst 3 tärningskast. " +
+                "För var slag avgör du vilka tärningar du vill spara till nästa kast och vilka " +
+                "tärningar du vill kasta om. Spara en tärning genom att klicka på den, tärningarna" +
+                " blir grön när du har sparat dem. Efter alla kast förs poängsumman in intill en av rubrikerna.";
+
+
         }
     }
 }
