0d1f3b22889d9423cceeacdaa7fd5d39ed4a10d2
Petter edlund <petter.edlund@gmail.com>
2019-09-23 10:11:29 +0200

    Test saker

--- YatzyGrupp2/Test/FormLabelTest.cs
+++ YatzyGrupp2/Test/FormLabelTest.cs
@@ -18,6 +18,12 @@ namespace YatzyGrupp2.Test
             
         }
         List<Label> testList = new List<Label>();
+        int xDif = 50;
+        int yDif = 30;
+        int edgeDif = 20;
+        int fontSize = 12;
+        string fontType = "Arial";
+        string extraSpace = "     ";
         private void FormLabelTest_Load(object sender, EventArgs e)
         {
             
@@ -32,11 +38,43 @@ namespace YatzyGrupp2.Test
             test2.Name = "lblTest";
             test2.Text = "Label test.";
             test2.BorderStyle = BorderStyle.FixedSingle;
-            testList.Add(testLabel);
-            testList.Add(test2);
-            Controls.Add(testList[0]);
-            Controls.Add(testList[1]);
+            //testList.Add(testLabel);
+            //testList.Add(test2);
+            //Controls.Add(testList[0]);
+            //Controls.Add(testList[1]);
+            int temp = 0;
+            for(int i = 0; i < 19; i++)
+            {
+                for(int o = 0; o < 5; o++)
+                {
+                    Label cellLabel = new Label();
+                    cellLabel.Name = "lbl" + Convert.ToString(temp);
+                    if(o == 0)
+                        cellLabel.Text = "lbl"+ extraSpace + Convert.ToString(temp);
+                    else
+                        cellLabel.Text = "lbl" + Convert.ToString(temp);
+                    cellLabel.Font = new System.Drawing.Font(fontType, fontSize, FontStyle.Bold);
+                    cellLabel.BorderStyle = BorderStyle.FixedSingle;
+                    cellLabel.Size = new Size(cellLabel.PreferredWidth, cellLabel.PreferredHeight);
+                    if(o == 0)
+                    {
+                        int t = extraSpace.Length + fontSize;
+
+                        cellLabel.Location = new Point((edgeDif + t) + (o * xDif), edgeDif + (i * yDif));
+                    }
+                    else
+                    {
+                        cellLabel.Location = new Point(edgeDif + (o * xDif), edgeDif + (i * yDif));
+                    }
+                    
+                    testList.Add(cellLabel);
+                }
+            }
             
+            for(int q = 0; q < testList.Count; q++)
+            {
+                Controls.Add(testList[q]);
+            }
         }
 
         private void Button1_Click(object sender, EventArgs e)
